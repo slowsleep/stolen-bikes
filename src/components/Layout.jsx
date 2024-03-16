3dbe35f4b3053ab27f24c83bbda7ea91267a1685
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-03-16 21:34:52 +0300

    Edit layout. Add adaptive container width depending on the pathname

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,16 @@
 import Container from "@mui/material/Container";
 import Header from "./Header/Header";
 import {Outlet} from "react-router-dom"
+import { useLocation } from 'react-router-dom';
 
 const Layout = () => {
+    let location = useLocation();
+
     return (
         <div>
             <Header />
             <main>
-                <Container maxWidth="sm">
+                <Container maxWidth={location.pathname === "/" ? "lg" : "sm"}>
                     <Outlet/>
                 </Container>
             </main>
