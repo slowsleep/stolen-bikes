be963456f3b2ccedafdeea570bffa0e06590ceed
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-03-17 18:42:41 +0300

    Edit adaptive container maxWigth for /cases and /officers pathnames

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import Container from "@mui/material/Container";
 import Header from "./Header/Header";
-import {Outlet} from "react-router-dom"
-import { useLocation } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Layout = () => {
     let location = useLocation();
@@ -10,8 +10,16 @@ const Layout = () => {
         <div>
             <Header />
             <main>
-                <Container maxWidth={location.pathname === "/" ? "lg" : "sm"}>
-                    <Outlet/>
+                <Container
+                    maxWidth={
+                        location.pathname === "/" ||
+                        location.pathname === "/cases" ||
+                        location.pathname === "/officers"
+                            ? "lg"
+                            : "sm"
+                    }
+                >
+                    <Outlet />
                 </Container>
             </main>
             <footer></footer>
